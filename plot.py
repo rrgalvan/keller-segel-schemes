f01ed27781ad7555d1e416791dc94facb71034f5
rrgalvan <rafael.rodriguez@uca.es>
2017-12-18 20:38:39 +0100

    Resalto en los pdf las diferencias de cada esquema con los anteriores

--- plot.py
+++ plot.py
@@ -11,11 +11,11 @@ This is a temporary script file.
 
 import matplotlib.pylab as plt
 
-filename = 'fileedp/plot.txt'
+filename = 'plot.txt'
 with open(filename) as f:
     lines = f.readlines()
     x = [line.split()[0] for line in lines]
     y = [line.split()[1] for line in lines]
     plt.plot(x, y, c='blue', linewidth=5)
-    plt.title("Energia")
+    plt.title("Max(u)")
     plt.show() 
